1a5e64e6350cbdccdaca54605e84f7da165fa900
Rodrigo Hurtado <rohulo@hotmail.es>
2015-08-03 15:07:43 -0500

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -37,13 +37,16 @@ var Quiz = sequelize.import(path.join(__dirname,"quiz"));
 exports.Quiz = Quiz;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success( function() {
-	Quiz.count().success( function(count){
+sequelize.sync().then( function() {
+	Quiz.count().then( function(count){
 		if(count === 0){
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
+			});
+			Quiz.create({ pregunta: 'Capital de portugal',
+						  respuesta: 'Lisboa'
 			})
-		.success( function(){ console.log("base de datos inicializada")});
+		.then( function(){ console.log("base de datos inicializada")});
 		};
 	});
 });
